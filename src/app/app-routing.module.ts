5197fcc03aa862526e5d25dc99a05f05f2d02d3a
Alfafis <ctpmmg2009_101@hotmail.com>
2020-04-19 15:42:25 -0300

    app adat routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomePage } from './home/home.page';
+import { RegisterComponent } from './register/register.component';
+
+imports: [
+  RouterModule.forRoot([
+    { path: '', component: HomePage },
+    { path: 'register', component: RegisterComponent }
+  ])
+]
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
 ];
 
 @NgModule({
